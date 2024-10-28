f218f69263a979c8fd6fe81d0333e0d5259d3b81
arcomajes <angelrose.comajes15@gmail.com>
2024-10-29 01:06:53 +0800

    merged from test and master

--- ASI.Basecode.WebApp/wwwroot/js/site.js
+++ ASI.Basecode.WebApp/wwwroot/js/site.js
@@ -1,14 +1,4 @@
-﻿let dpicn = document.querySelector(".dpicn");
-let dropdown = document.querySelector(".dropdown");
-
-dpicn.addEventListener("click", () => {
-    dropdown.classList.toggle("dropdown-open");
-})
-
-
-
-
-
+﻿
 // start: Popper
 const popperInstance = {}
 document.querySelectorAll('.dropdown').forEach(function (item, index) {
